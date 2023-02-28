d265d4336f9f6e3c44bcc13de37117fdff3b1ea6
farm rakpong <farmza560@gmail.com>
2023-02-28 22:12:23 +0700

    Add project files.

---
+++ BasicProgramming/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10;
+            int y = 20;
+            float a = 10.20f;
+            float b = 10f;
+
+            double number1;
+            number1 = 500;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(number1);
+            Console.ReadKey();
+        }
+    }
+}
